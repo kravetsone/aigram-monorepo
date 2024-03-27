7fa663af996f45efa184caaa348a987a26c29cde
kravetsone <57632712+kravetsone@users.noreply.github.com>
2024-03-27 20:41:12 +0300

    refactor: user auth

--- packages/server/src/routes/user.ts
+++ packages/server/src/routes/user.ts
@@ -1,25 +1,8 @@
-import bearer from "@elysiajs/bearer";
-import { eq } from "drizzle-orm";
 import Elysia from "elysia";
-import { db, users } from "../db";
-import { jwtElysia } from "../services";
+import { authElysia } from "../services";
 
 export const userRoutes = new Elysia()
-	.use(bearer())
-	.use(jwtElysia)
-	.derive(async ({ bearer, jwt, error }) => {
-		const jwtData = await jwt.verify(bearer);
-		if (!jwtData) throw error("Unauthorized");
-
-		const [user] = await db
-			.select()
-			.from(users)
-			.where(eq(users.id, jwtData.id));
-
-		return {
-			user,
-		};
-	})
+	.use(authElysia)
 	.get("/user", ({ user }) => {
 		return {
 			name: user.name,
--- packages/server/src/services.ts
+++ packages/server/src/services.ts
@@ -1,5 +1,8 @@
+import bearer from "@elysiajs/bearer";
 import jwt from "@elysiajs/jwt";
+import { eq } from "drizzle-orm";
 import Elysia, { t } from "elysia";
+import { db, users } from "./db";
 
 export const jwtElysia = new Elysia().use(
 	jwt({
@@ -9,3 +12,20 @@ export const jwtElysia = new Elysia().use(
 		}),
 	}),
 );
+
+export const authElysia = new Elysia()
+	.use(bearer())
+	.use(jwtElysia)
+	.derive({ as: "scoped" }, async ({ bearer, query, jwt, error }) => {
+		const jwtData = await jwt.verify(bearer ?? query.token);
+		if (!jwtData) throw error("Unauthorized");
+
+		const [user] = await db
+			.select()
+			.from(users)
+			.where(eq(users.id, jwtData.id));
+
+		return {
+			user,
+		};
+	});
